8d5c130df01624a6f79b28434e9dafa65b0022d0
mottetm <m.mottet@outlook.com>
2019-06-20 20:56:47 +0200

    Update user_test.sql

--- user_test.sql
+++ user_test.sql
@@ -33,16 +33,9 @@ VALUES (
     'create', 'user', '{"username": "johnS", "email": "john.smith@email.com", "password": "a1b2c3d4"}', now() + INTERVAL '60 second'
 );
 
---INSERT INTO "sourcing"."event" ( 
---    "event", "data", "timestamp"
---) 
---VALUES (
---    'rollback', '{"id": 3}', now() + INTerval '60 second'
---);
-
 INSERT INTO "sourcing"."event" ( 
     "event", "data", "timestamp"
 ) 
 VALUES (
-    'rollback', jsonb_set('{}'::JSONB, '{timestamp}'::TEXT[], to_jsonb((now() + INTERVAL '30 second')::TIMESTAMP WITHOUT TIME ZONE::TEXT)), now() + INTERVAL '90 second'
+    'rollback', '{"id": 3}', now() + INTerval '60 second'
 );
